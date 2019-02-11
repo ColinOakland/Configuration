2dcd451ad137b6e04086f071cf61741b852802f6
Colin <colinmurphy@oakland.edu>
2019-02-11 13:45:52 -0500

    Implemented the method for printing the song 99 bottles of beer on the wall

--- BeerSong.java
+++ BeerSong.java
@@ -7,8 +7,15 @@ public class BeerSong{
 
 	public void Ninety_Nine_Bottles_of_Beer()
 	{
-
-	}
+	int bottles=99;
+    	while(bottles-1>0)
+    	{
+        	System.out.println(bottles + " Bottles of beer on the wall, " + bottles + " bottles of beer. Take one down, pass it around ");
+        	bottles--;
+        	System.out.println(bottles + " Bottles of beer on the wall");
+    	}
+    	System.out.print("Take one down, pass it around, no more bottles of beer on the wall");
+    	}
 
 
 }
